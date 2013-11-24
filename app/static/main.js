0290d9c87cd34dc198461dfdc5c61bedced3108c
sprin <sprin>
2013-11-23 20:54:02 -0800

    Use real readthedocs API again

--- app/static/main.js
+++ app/static/main.js
@@ -10,10 +10,10 @@ define(function (require) {
       default_endpoint: 'sections',
       endpoints: {
         sections: {
-          data_type: 'json',
-          //default_params: {format: 'jsonp'},
-          api_url: 'http://searchtd.dev:8000/api/v2/search/section/',
-          record_url:
+          data_type: 'jsonp',
+          default_params: {format: 'jsonp'},
+          api_url: 'http://readthedocs.org/api/v2/search/section/',
+          content_url_format:
             'http://{{domain}}.readthedocs.org/en/'
             + '{{version}}/{{path}}.html#{{page_id}}',
           param_map: {
