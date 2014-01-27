78b4d0c8d1807bec686ea3de345a1f6e182b40aa
sprin <sprin>
2014-01-27 01:23:44 -0800

    Add hint logic and display rule for domain completion hint

--- app/static/src/hint_display.js
+++ app/static/src/hint_display.js
@@ -4,15 +4,17 @@ define(function(require) {
   require('bootstrap');
   var hint_rules = require('hint_rules');
 
+  window.hints_shown_this_session = [];
+
   var check_hint_rules = function(tracking_params) {
-    var hints = [];
+    var hints = {};
 
     // Check each hint rule, and if the hint rule matches, push the hint
     // meta into the hints array.
-    _.each(hint_rules, function(hint_rule) {
+    _.each(hint_rules, function(hint_rule, hint_name) {
       var hint_meta = hint_rule(tracking_params);
       if (hint_meta) {
-        hints.push(hint_meta);
+        hints[hint_name] = hint_meta;
       }
     });
 
@@ -24,20 +26,49 @@ define(function(require) {
     hints_meta = check_hint_rules(tracking_params);
 
     // Apply display rules for every matching hint.
-    _.each(hints_meta, function(hint_meta) {
-      var $el = $(hint_meta.el);
-      // Invoke the "hint_type" (tooltip, etc) method on the selected el.
-      // Pass the hint options to the method.
-      $el[hint_meta.hint_method](hint_meta.hint_options);
-      console.log(hint_meta)
-      console.log($el)
-      console.log($el[hint_meta.hint_method]);
-      $el[hint_meta.hint_method](hint_meta.hint_options);
+    _.each(hints_meta, function(hint_meta, hint_name) {
+
+      // Don't show same hint more than once in session
+      if (!_.contains(hints_shown_this_session, hint_name)) {
+
+
+        var $el = $(hint_meta.el);
+        // Invoke the "hint_type" (tooltip, etc) method on the selected el.
+        // Pass the hint options to the method.
+        if (hint_meta.hint_method === 'tooltip') {
+
+          setTimeout(function() {
+            hints_shown_this_session.push(hint_name);
+            $el.tooltip(hint_meta.hint_options);
+            $el.tooltip('show');
+          }, 100);
+
+          Backbone.on(hint_meta.remove_event, _.once(function() {
+            $el.tooltip('destroy');
+          }));
+        } else if (hint_meta.hint_method === 'inline') {
+          setTimeout(function() {
+            hints_shown_this_session.push(hint_name);
+            var hint_el = '<p class="tooltip-inner tooltip-inline">' + hint_meta.hint_options.title + '</p>';
+            $(hint_el)
+              .hide()
+              .appendTo($el)
+              .slideDown('slow');
+          }, 100);
+
+          Backbone.on(hint_meta.remove_event, _.once(function() {
+            $hint_el.remove();
+          }));
+        }
+
+      }
     });
 
   };
 
-  return display_hints;
+  var throttled_display_hints = _.debounce(display_hints, 500);
+
+  return throttled_display_hints;
 
 });
 
--- app/static/src/hint_rules.js
+++ app/static/src/hint_rules.js
@@ -4,21 +4,45 @@ define(function() {
     // Hint Rules take a hash of user tracking parameters to offer
     // hints to the user based on previous interactions.
     // Parameters:
-    // - visit_number
+    // - visits
     // - actions_performed (array)
     //
     // Returns a hash containing the metadata to display the hint.
     // For example:
     //
-    incremental_hint: function(options) {
-      var visit_number = options.visit_number;
-      if (visit_number === 1) {
+    search_hint: function(options) {
+      var visits = options.visits;
+      var searches = options.searches;
+      if (
+        visits === 1
+        && _.isEmpty(searches)
+      ) {
         return {
           "hint_method": "tooltip",
           "el": ".search-input-container",
+          "remove_event": "content_loaded",
           "hint_options": {
-            "title": "Incremental search! Start typing...",
+            "title": "Start searching!",
             "placement": "bottom",
+            "trigger": "manual",
+          }
+        };
+      }
+     },
+
+    domain_completion_hint: function(options) {
+      var searches = options.searches;
+      var domain_completions = options.domain_completions;
+      if (
+        !_.isEmpty(searches)
+        && _.isEmpty(domain_completions)
+      ) {
+        return {
+          "hint_method": "inline",
+          "el": "[data-view-name='suggestion-panel-container']",
+          "remove_event": "domain_completion",
+          "hint_options": {
+            "title": "Hint! You can also tab-complete project filters",
           }
         };
       }
